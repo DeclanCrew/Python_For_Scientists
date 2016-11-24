8aea7cb88a8716162c452f332c0e3599e297430e
Deaglan Crew <d.crew@liv.ac.uk>
2016-11-24 02:32:53 +0000

    Added solutions

--- learncsv.py
+++ learncsv.py
@@ -7,5 +7,12 @@ def read_valid_universities (file, removeEmpty):
     if removeEmpty is set to True, it will skip entries with no data on part
     time students.
     """
-    return
+    output = []
+    with open(file) as csvFile:
+        for entry in csv.DictReader(csvFile):
+            if removeEmpty and entry["Part time"] == "None":
+                continue
+            output.append(entry)
+    return output
 
+read_valid_universities("uniData.csv", False)
